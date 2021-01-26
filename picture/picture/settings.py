697dffe474741391f47a4f669bba20863135e75e
petro1233 <piter.golius@gmail.com>
2021-01-26 14:03:37 +0300

    end

--- picture/picture/settings.py
+++ picture/picture/settings.py
@@ -78,9 +78,10 @@ WSGI_APPLICATION = 'picture.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
+
 
 
 # Password validation
